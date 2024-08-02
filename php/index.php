185ef056ad8a638c08f56407762f3df97958dda5
gut1nh0 <augusto.ferrari.senai404@gmail.com>
2024-08-02 09:50:06 -0300

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<?php
+$nome = 'Augusto';
+$escola = 'SESI Cerquilho';
+$CE = 428;
+$curso = 'Desenvolvimento de Sistemas';
+$media = 6.5;
+
+echo 'Olá bem vindo ao SESI-SP <br>';
+
+echo "$nome <br>";
+echo "escola <br>";
+echo "$CE <br>";
+echo "$curso <br>";
+echo "$media <br>";
+
+echo "Olá $nome, seja bem vindo ao $escola $CE, sua nota no curso de $curso é: $media"
+
+?>
