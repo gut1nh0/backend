fc21a9492bf3aaeddaa8eff2938e4d8e42bd7a1a
gut1nh0 <augusto.ferrari.senai404@gmail.com>
2024-10-02 13:50:30 -0300

    html_php[

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php $nome = 'Augusto'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Bem vindo <?php echo $nome;?></h1>
+    <h1><?php echo 'Olá meu Senai';?></h1>
+</body>
+</html>
