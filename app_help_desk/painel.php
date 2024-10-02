ccbfbf9e052422f1c5f0b54e0f2d06bc6e747896
gut1nh0 <augusto.ferrari.senai404@gmail.com>
2024-10-02 13:53:17 -0300

    app_help_desk

---
+++ app_help_desk/painel.php
@@ -0,0 +1,42 @@
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>App Help Desk</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    
+    </style>
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="./index.php">
+        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        App Help Desk
+      </a>
+    </nav>
+
+    <div class="container">    
+      <div class="row">
+
+        <div class="card-home">
+          <div class="card">
+            <div class="card-header">
+              Menu
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-6 d-flex justify-content-center">
+                  <a href="./abrir_chamado.php"><img src="formulario_abrir_chamado.png" width="70" height="70"></a>
+                </div>
+                <div class="col-6 d-flex justify-content-center">
+                  <a href="./consultar_chamado.php"><img src="formulario_consultar_chamado.png" width="70" height="70"></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </body>
+</html>
