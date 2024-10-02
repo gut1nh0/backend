ab245482d85c7528bab093b8b55cc2ec12f33729
gut1nh0 <augusto.ferrari.senai404@gmail.com>
2024-10-02 16:35:44 -0300

    valida_login 2/10

--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -1,12 +1,24 @@
 <?php 
 
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
-echo "<hr>";
+$usuario_autenticado = false;
 
-echo "E-mail: ";
-echo $_POST['email'];
-echo "<br>";
-echo $_POST['senha'];
+$usuario_cadastrado = [
+    ['email' => 'admin@senai.br',
+    'senha' => 12345]
+];
+
+foreach ($usuario_cadastrado as $user){
+    if ($user ['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+        
+        $usuario_autenticado = true;
+    }
+}
+
+if ($usuario_autenticado == true){
+    header ('Location: painel.php?login=sucesso');
+}
+
+else{
+    header('Location: index.php?login=erro');
+}
 ?>
