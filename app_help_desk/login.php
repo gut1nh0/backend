775bffd1077aa5868a0e380840cde4eb4daaf02f
Augusto Ferrari <augusto.ferrari.senai404@gmail.com>
2024-10-25 09:48:09 -0300

    Delete app_help_desk directory

--- app_help_desk/login.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-
-//echo $_SESSION['autenticado'];
-if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM'){
-  header ('Location: index.php?login=erro2');
-}
-?>
